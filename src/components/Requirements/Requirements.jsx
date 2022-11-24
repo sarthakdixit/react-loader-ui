f8327c485d79f464b71850310d79d631977aa7ae
sarthakdixit <dsarthak12345@gmail.com>
2022-11-24 19:42:33 +0530

    Coin UI

--- src/components/Requirements/Requirements.jsx
+++ src/components/Requirements/Requirements.jsx
@@ -1,5 +1,6 @@
 import { Default } from "../Default/Default";
 import { Spinner1 } from "../Spinner1/Spinner1";
+import { Coin } from "../Coin/Coin";
 
 export const Requirements = ({name, size, color1, color2, color3, color4, width}) => {
     const type = typeof name === "string" ? name : "default";
@@ -9,6 +10,8 @@ export const Requirements = ({name, size, color1, color2, color3, color4, width}
             {type === "default" ? <Default size={size} color1={color1} color2={color2} color3={color3} color4={color4} width={width} /> : null}
 
             {type === "spinner1" ? <Spinner1 size={size} color1={color1} width={width} /> : null}
+
+            {type === "coin" ? <Coin size={size} color1={color1} width={width} /> : null}
         </>
     );
 }
