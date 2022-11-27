382ace7f4b2a4be0d03e943441476a803ab31a1e
sarthakdixit <dsarthak12345@gmail.com>
2022-11-27 09:27:34 +0530

    storybook

---
+++ src/stories/Requirements.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {storiesOf} from "@storybook/react";
+import { Requirements } from '../components/Requirements/Requirements';
+
+const stories = storiesOf("App Test", module);
+
+stories.add("App", () => {
+    return (<Requirements name="fan" />)
+})
